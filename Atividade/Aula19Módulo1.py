be62a253d1cbaeba0d90385d8cc864f684681a8a
Joao Vitor Cunha Chinato <116226528+vitorpts@users.noreply.github.com>
2023-05-04 16:34:15 -0300

    Atividade desenvolvida no VSCode
    - Luciana
    - Josean
    - Luiz Gustavo
    - João Vitor

---
+++ Atividade/Aula19Módulo1.py
@@ -0,0 +1,11 @@
+from Biblioteca import *
+
+num1=int(input('Digite o primeiro numero: '))
+num2=int(input('Digite o segundo numero: '))
+print ('\nO resultado da soma é: ',soma(num1,num2))
+print ('\nO resultado da multiplicação é: ',multiplicacao(num1,num2))
+print ('\nO resultado da divisão é: ',divisao (num1,num2))
+print ('\nO resultado da subtração é: ',subtracao(num1,num2))
+print ('\nO resultado da divisão inteira é: ',divisao_int (num1,num2))
+print ('\nO resultado do resto da divisão é: ',divisao (num1,num2))
+print ("\nMuito obrigado, que você tenha um excelente dia!!")
